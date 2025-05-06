6b3c98e329b38908362ab7559d983ac81d81a678
Rohan Bhattacharya <Rohan.Bhattacharya@dhsc.gov.uk>
2025-05-06 16:30:15 +0100

    minor changes to chart function to improve visualisation

--- charts.R
+++ charts.R
@@ -45,15 +45,46 @@ generate_stacked_charts <- function(aggregate_data) {
   apps_data <- prepare_stacked_chart_data(aggregate_data, "Apps")
   grants_data <- prepare_stacked_chart_data(aggregate_data, "Grants")
 
+  # Step 1: Create new fill column
+  apps_data <- apps_data %>%
+    dplyr::mutate(
+      fill_group = interaction(Flow_Type, Worker_Type),
+      fill_group = factor(fill_group, levels = c(
+        "In Country.Senior Care Worker",
+        "Out of Country.Senior Care Worker",
+        "In Country.Care Worker",
+        "Out of Country.Care Worker"
+      ))
+    )
+
+  grants_data <- grants_data %>%
+    dplyr::mutate(
+      fill_group = interaction(Flow_Type, Worker_Type),
+      fill_group = factor(fill_group, levels = c(
+        "In Country.Senior Care Worker",
+        "Out of Country.Senior Care Worker",
+        "In Country.Care Worker",
+        "Out of Country.Care Worker"
+      ))
+    )
+
   apps_dates <- sort(unique(apps_data$Period_End))
   apps_breaks <- apps_dates[seq(1, length(apps_dates), by = 2)]  # every 2nd date
 
-  apps_plot <- ggplot2::ggplot(apps_data, ggplot2::aes(x = Period_End, y = Total, fill = interaction(Flow_Type, Worker_Type))) +
+  custom_colors <- c(
+    "Out of Country.Care Worker" = "orange",
+    "Out of Country.Senior Care Worker" = "yellow",
+    "In Country.Care Worker" = "blue",
+    "In Country.Senior Care Worker" = "grey"
+  )
+
+  apps_plot <- ggplot2::ggplot(apps_data, ggplot2::aes(x = Period_End, y = Total, fill = fill_group)) +
     ggplot2::geom_bar(stat = "identity", position = "stack") +
     ggplot2::scale_x_date(
       breaks = apps_breaks,
       date_labels = "%d %b %Y"
     ) +
+    ggplot2::scale_fill_manual(values = custom_colors) +
     ggplot2::labs(title = "Applications: In & Out of Country", x = "Period", y = "Total", fill = "Flow & Worker Type") +
     ggplot2::theme_minimal() +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
@@ -61,12 +92,13 @@ generate_stacked_charts <- function(aggregate_data) {
   grants_dates <- sort(unique(grants_data$Period_End))
   grants_breaks <- apps_dates[seq(1, length(grants_dates), by = 2)]  # every 2nd date
 
-  grants_plot <- ggplot2::ggplot(grants_data, ggplot2::aes(x = Period_End, y = Total, fill = interaction(Flow_Type, Worker_Type))) +
+  grants_plot <- ggplot2::ggplot(grants_data, ggplot2::aes(x = Period_End, y = Total, fill = fill_group)) +
     ggplot2::geom_bar(stat = "identity", position = "stack") +
     ggplot2::scale_x_date(
       breaks = grants_breaks,
       date_labels = "%d %b %Y"
     ) +
+    ggplot2::scale_fill_manual(values = custom_colors) +
     ggplot2::labs(title = "Grants: In & Out of Country", x = "Period", y = "Total", fill = "Flow & Worker Type") +
     ggplot2::theme_minimal() +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
